0869508e8ed8fc4a9748ad01243b92f07c6e278f
Dinesh SThapit <todxs@hotmail.com>
2020-10-26 20:59:15 -0600

    added new code for conversion

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,15 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            // 2 ^4 = 16
+            // 2 ^2 = 4
+            // 2 ^0 = 1
+            byte x = 0b_1000; 
+
+            int b = x << 5;
+            Console.WriteLine($"{Convert.ToString(b, toBase: 2)}");  // output: 1111000100000000
+
+         
         }
     }
 }
